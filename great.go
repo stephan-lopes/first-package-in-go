4907ecdf776753573814c8436e7a7fb1139c9ab6
Keven S. Lopes <kstephanl1908@gmail.com>
2021-11-16 00:12:40 -0300

    :sparkles: adding description constant

--- great.go
+++ great.go
@@ -2,7 +2,7 @@ package firstpackageingo
 
 import "fmt"
 
-var Description = "This is my first package in go!"
+const Description = "This is my first package in go!"
 
 func Hello() {
 	fmt.Println("Hello World!")
