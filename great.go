c72bc722219c939a6e47bb3f4e4809e333e7de82
Keven S. Lopes <kstephanl1908@gmail.com>
2021-11-17 00:00:16 -0300

    :sparkles: Adding Lion var and Felidae Struct

--- great.go
+++ great.go
@@ -4,6 +4,17 @@ import "fmt"
 
 const Description = "This is my first package in go!"
 
+var Lion = "Leo"
+
+type Felidae struct {
+	Name  string
+	Color string
+}
+
+func (f Felidae) String() string {
+	return fmt.Sprintf("The lion %s and color %s is a large mammal of the Felidae family", f.Name, f.Color)
+}
+
 func Hello() {
 	fmt.Println("Hello World!")
 }
